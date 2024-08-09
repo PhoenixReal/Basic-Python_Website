649f4a7208ae4c428d2cba035d01ba7efa47cb76
Phoenix <thanhvatly893@gmail.com>
2024-08-09 22:57:36 +0700

    Added login page (which is making a better)

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template
-from app import app 
+from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+@app.route('/login')
 def index():
     user = {'username': 'Phoenix'}
     posts = [
@@ -16,4 +18,7 @@ def index():
         }
     ]
     return render_template('index.html', title='Trang chủ', user=user, posts=posts)
-
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Đăng nhập', form=form)
